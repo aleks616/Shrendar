6f9e0aac63ea88ed3b38d83e912f96c1568afb93
aleks616 <aleksjankowiak5230@gmail.com>
2025-04-09 18:45:44 +0200

    fixed genre similarity function, added test

--- backend/src/main/kotlin/org/aleks616/shrendar/GenreSimilarity.kt
+++ backend/src/main/kotlin/org/aleks616/shrendar/GenreSimilarity.kt
@@ -10,12 +10,16 @@ object GenreSimilarity{
     fun getGenreSimilarity(genre1:Int,genre2:Int):Double{
         val genre1String=genre1.toString()
         val genre2String=genre2.toString()
+        return getGenreSimilarity(genre1String,genre2String)
+    }
+
+    fun getGenreSimilarity(genre1:String,genre2:String):Double{
         var value=0.0
         for(i in 0..6){
-            if(i<3) value+=abs(genre1String[i]-genre2String[i])
-            else value+=0.667*abs(genre1String[i]-genre2String[i])
+            if(i<3) value+=abs(genre1[i]-genre2[i])
+            else value+=0.667*abs(genre1[i]-genre2[i])
         }
-        return value
+        return "%.2f".format(value).toDouble()
     }
 
 }
