d0b4fb5a1a78f496b4b3ea1d0ebe3f029cb45341
aleks5230 <aleksjankowiak5230@gmail.com>
2026-07-18 15:13:22 +0200

    Upgraded java from 17 to 26

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -10,7 +10,7 @@ version="0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion=JavaLanguageVersion.of(17)
+        languageVersion=JavaLanguageVersion.of(26)
     }
 }
 
@@ -31,7 +31,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("com.h2database:h2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
