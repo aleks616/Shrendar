e9984670b10d3ef3a775a9b570679f61bddedb70
aleks5230 <aleksjankowiak5230@gmail.com>
2026-08-08 22:58:29 +0200

    Endpoints

--- backend/src/main/kotlin/org/aleks616/shrendar/contribution/controller/ContributionController.kt
+++ backend/src/main/kotlin/org/aleks616/shrendar/contribution/controller/ContributionController.kt
@@ -8,8 +8,10 @@ import org.aleks616.shrendar.security.RateLimiter
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.parameters.P
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -66,4 +68,20 @@ class ContributionController (
 
         return ResponseEntity.ok("Addition reverted successful")
     }
+
+    @PostMapping("/requested-by/{id}")
+    fun getContributionsByRequestingUser(@PathVariable id:Int){
+
+    }
+
+    @PostMapping("/confirmed-by/{id}")
+    fun getContributionsByConfirmingUser(@PathVariable id:Int){
+
+    }
+
+    @PostMapping("/table/{table}")
+    fun getContributionsByTableName(@PathVariable table:String){
+        
+    }
+
 }
