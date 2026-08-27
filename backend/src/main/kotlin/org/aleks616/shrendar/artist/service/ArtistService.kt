042ed172da1b757a42563c007595e1f5ab5a8526
aleks5230 <aleksjankowiak5230@gmail.com>
2026-08-27 18:23:20 +0200

    optimized imports

--- backend/src/main/kotlin/org/aleks616/shrendar/artist/service/ArtistService.kt
+++ backend/src/main/kotlin/org/aleks616/shrendar/artist/service/ArtistService.kt
@@ -9,9 +9,6 @@ import org.aleks616.shrendar.contribution.model.Action
 import org.aleks616.shrendar.contribution.model.Contribution
 import org.aleks616.shrendar.contribution.repository.ContributionRepository
 import org.aleks616.shrendar.exception.ContributionLimitExceededException
-import org.aleks616.shrendar.genre.model.Genre
-import org.aleks616.shrendar.genre.model.GenreDto
-import org.aleks616.shrendar.genre.model.GenreDto1
 import org.aleks616.shrendar.user.model.User
 import org.aleks616.shrendar.user.model.UsersArtists
 import org.aleks616.shrendar.user.repository.UserArtistRepository
