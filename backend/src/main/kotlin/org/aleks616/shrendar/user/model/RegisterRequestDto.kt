23e6d1e1af61749aec1bec4454c6d3553b6b429f
mantikora-pl <aleksjankowiak5230@gmail.com>
2026-09-20 20:12:21 +0200

    language code backend adjustments

--- backend/src/main/kotlin/org/aleks616/shrendar/user/model/RegisterRequestDto.kt
+++ backend/src/main/kotlin/org/aleks616/shrendar/user/model/RegisterRequestDto.kt
@@ -1,10 +1,12 @@
 package org.aleks616.shrendar.user.model
 
+import org.aleks616.shrendar.common.model.SupportedLanguages
 import java.io.Serializable
 
 data class RegisterRequestDto(
     val login:String,
     val displayName:String,
     val email:String,
-    val password:String
+    val password:String,
+    val language:SupportedLanguages?=SupportedLanguages.EN
 ):Serializable
