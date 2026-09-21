e02c8296272e29efc9838e82d84814e45ae414e8
aleks616 <aleksjankowiak5230@gmail.com>
2026-09-22 01:34:02 +0200

    Register form react + heroui configuration

--- client/webApp/src/index.tsx
+++ client/webApp/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import {GenreClient} from 'sharedLogic'
+import {GenreClient} from 'sharedLogic'
 import {Register} from './components/Register/Register.tsx'
 
 const rootElement=document.getElementById('root')
 if(!rootElement) throw new Error('Failed to find the root element')
 
-/*GenreClient.getInstance().getAll()
-    .then((genres) => console.log(genres))*/
+GenreClient.getInstance().getAll()
+    .then((genres) => console.log(genres))
 
 ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
