349c0273720219114bf28dd48b8f04e444d3ad21
aleks616 <aleksjankowiak5230@gmail.com>
2026-09-23 15:08:59 +0200

    strings

--- client/iosApp/iosApp/RegisterView.swift
+++ client/iosApp/iosApp/RegisterView.swift
@@ -13,292 +13,292 @@ import SwiftUI
 import SwiftUIOTPEntry
 
 struct RegisterView: View {
-	let lang = Locale.current.language.languageCode ?? "EN"
-	private enum Field: Int, CaseIterable {
-		case email,login,password,confirmPassword
-	}
-	@State private var email: String = ""
-	@State private var login: String = ""
-	@State private var password: String = ""
-	@State private var confirmPassword: String = ""
-	@State private var code: String = ""
-	
-	@FocusState private var focusedField: Field?
-	@State var isDismissKeyboard: Bool = false
-	
-	@State private var errorText: String? = ""
-	@State private var codeSent: Bool = false
-	@State private var timerOn: Bool = false
-	@State private var resendCountdown: Int = 60
-	@State private var confirmed: Bool = false
+  let lang = Locale.current.language.languageCode ?? "EN"
+  private enum Field: Int, CaseIterable {
+    case email, login, password, confirmPassword
+  }
+  @State private var email: String = ""
+  @State private var login: String = ""
+  @State private var password: String = ""
+  @State private var confirmPassword: String = ""
+  @State private var code: String = ""
 
-	let timer: Timer.TimerPublisher = Timer.publish(
-		every: 1.0,
-		on: .main,
-		in: .common
-	)
-	
-	let model: ModelUISwiftUIOTPEntry = .init(
-		font: .systemFont(ofSize: 20),
-		textAccessibilityForEmptyBox: "Empty box",
-		textAccessibilityPosition: "Position",
-		count: 6,
-		spacing: 8,
-		colorFocused: .red,
-		colorEmpty: .gray,
-		colorFill: .green,
-		size: 50
-	)
+  @FocusState private var focusedField: Field?
+  @State var isDismissKeyboard: Bool = false
 
-	var body: some View {
-		VStack {
-			Text(localize(key: "create_account"))
-				.font(.system(size: 28.0, weight: .bold))
-			Text(localize(key: "sign_up_to_continue"))
-				.font(.system(size: 20.0))
-			Spacer()
-			Form {
-				TextField(
-					localize(key: "email_address"),
-					text: $email
-				)
-				.keyboardType(.emailAddress)
-				.textContentType(.emailAddress)
-				.font(.system(size: 24.0))
-				.focused($focusedField, equals: .email)
-				.autocorrectionDisabled()
+  @State private var errorText: String? = ""
+  @State private var codeSent: Bool = false
+  @State private var timerOn: Bool = false
+  @State private var resendCountdown: Int = 60
+  @State private var confirmed: Bool = false
 
-				TextField(
-					localize(key: "login"),
-					text: $login
-				)
-				.textContentType(.username)
-				.font(.system(size: 24.0))
-				.focused($focusedField, equals: .login)
-				.autocorrectionDisabled()
+  let timer: Timer.TimerPublisher = Timer.publish(
+    every: 1.0,
+    on: .main,
+    in: .common
+  )
 
-				SecureField(
-					localize(key: "password"),
-					text: $password
-				)
-				.textContentType(.password)
-				.focused($focusedField, equals: .password)
-				.font(.system(size: 24.0))
+  let model: ModelUISwiftUIOTPEntry = .init(
+    font: .systemFont(ofSize: 20),
+    textAccessibilityForEmptyBox: "Empty box",
+    textAccessibilityPosition: "Position",
+    count: 6,
+    spacing: 8,
+    colorFocused: .red,
+    colorEmpty: .gray,
+    colorFill: .green,
+    size: 50
+  )
 
-				SecureField(
-					localize(key: "re_enter_password"),
-					text: $confirmPassword
-				)
-				.textContentType(.password)
-				.focused($focusedField, equals: .confirmPassword)
-				.font(.system(size: 24.0))
+  var body: some View {
+    VStack {
+      Text(localize(key: "create_account"))
+        .font(.system(size: 28.0, weight: .bold))
+      Text(localize(key: "sign_up_to_continue"))
+        .font(.system(size: 20.0))
+      Spacer()
+      Form {
+        TextField(
+          localize(key: "email_address"),
+          text: $email
+        )
+        .keyboardType(.emailAddress)
+        .textContentType(.emailAddress)
+        .font(.system(size: 24.0))
+        .focused($focusedField, equals: .email)
+        .autocorrectionDisabled()
 
-			}.toolbar {
-				ToolbarItem(placement: .keyboard) {
-					Button("Done") {
-						focusedField = nil
-					}
-				}
-			}
-			
-			Text(errorText ?? "").foregroundStyle(.red)
+        TextField(
+          localize(key: "login"),
+          text: $login
+        )
+        .textContentType(.username)
+        .font(.system(size: 24.0))
+        .focused($focusedField, equals: .login)
+        .autocorrectionDisabled()
 
-			Button(action: validateFields) {
-				Text(localize(key: "sign_up"))
-			}.buttonStyle(.glass)
-				.disabled(
-					email.isEmpty || login.isEmpty || password.isEmpty
-						|| confirmPassword.isEmpty || (resendCountdown > 0 && codeSent ) || confirmed
-				)
+        SecureField(
+          localize(key: "password"),
+          text: $password
+        )
+        .textContentType(.password)
+        .focused($focusedField, equals: .password)
+        .font(.system(size: 24.0))
 
-			if codeSent {
-				Text(localize(key: "verification_code_sent"))
-				Text("resend code in \(resendCountdown)")
-				Button(action: createAccount){
-					Text("Resend code")
-				}
-				.disabled(resendCountdown > 0)
-				
-				ViewSwiftUIOTPEntry(model: model, number: $code, isDismissKeyboard: $isDismissKeyboard)
-				Button(action:confirmAccount){
-					Text("confirm account")
-				}
-				.buttonStyle(.glass)
-				.disabled(code.count<6)
-			}
-			if confirmed {
-				Text("Your account has been confirmed. You can log into your account")
-			}
+        SecureField(
+          localize(key: "re_enter_password"),
+          text: $confirmPassword
+        )
+        .textContentType(.password)
+        .focused($focusedField, equals: .confirmPassword)
+        .font(.system(size: 24.0))
 
-			LabelledDivider(label: localize(key: "or"))
-			//			GoogleSignInButton(
-			//				scheme: .light,
-			//				state: .normal,
-			//				action: {handleGoogleSignInButton()})
-			//			.frame(width: 280, height: 45)
-			//
-			//			SignInWithAppleButton(.continue){
-			//				request in request.requestedScopes=[.email]
-			//			} onCompletion: { result in
-			//				switch result {
-			//					case .success(let authorization):
-			//						print("\(localize(key: "authorization_successful")): \(authorization)")
-			//					case .failure(let error):
-			//						print("\(localize(key: "authorization_failed"))): \(error.localizedDescription)")
-			//				}
-			//			}.frame(width: 280, height: 45)
+      }.toolbar {
+        ToolbarItem(placement: .keyboard) {
+          Button(localize(key: "done")) {
+            focusedField = nil
+          }
+        }
+      }
 
-			//Text(localize(key: "special_sign_in_later"))
-			//Spacer()
-			HStack {
-				Text(localize(key: "already_have_account"))
-				Button(localize(key: "sign_in")) {}
-			}
+      Text(errorText ?? "").foregroundStyle(.red)
 
-		}
-		.padding(.top, 15)
-		.onReceive(
-			timer.autoconnect(),
-			perform: { _ in
-				if timerOn {
-					if resendCountdown > 0 {
-						resendCountdown = resendCountdown - 1
-					}
-					else {
-						resendCountdown = 60
-						timerOn = false
-					}
-				}
-			}
-		)
-	}
-	
-	func validateFields() {
-		Task{
-			let registerValidator = RegisterValidator()
-			do {
-				let loginValid = try await registerValidator.validateLogin(
-					login: login
-				)
-				if loginValid != nil {
-					errorText = localize(key: loginValid!)
-					return
-				}
-				
-				let emailValid = try await registerValidator.validateEmail(
-					email: email
-				)
-				if emailValid != nil {
-					errorText = localize(key: emailValid!)
-					return
-				}
-				
-				if password != confirmPassword {
-					errorText = localize(key: "passwords_dont_match")
-					return
-				}
-				
-				let passwordValid = registerValidator.isPasswordValid(
-					password: password
-				)
-				if passwordValid != true {
-					errorText = localize(key: "invalid_password")
-				}
-				errorText = ""
-			}
-			catch let error {
-				print(error)
-				return
-			}
-			createAccount()
-		}
-	}
+      Button(action: validateFields) {
+        Text(localize(key: "sign_up"))
+      }.buttonStyle(.glass)
+        .disabled(
+          email.isEmpty || login.isEmpty || password.isEmpty
+            || confirmPassword.isEmpty || (resendCountdown > 0 && codeSent) || confirmed
+        )
 
-	func createAccount() {
-		Task {
-			do {
-				let langCode: String = lang.identifier.uppercased()
-				let registerRequest = RegisterRequestDto(
-					login: login,
-					displayName: login,
-					email: email,
-					password: password,
-					language: langCode
-				)
-				let result = try await RegisterClient().register(
-					request: registerRequest
-				)
-				if result == "verification_code_sent" {
-					codeSent = true
-					timerOn = true
-				}
+      if codeSent {
+        Text(localize(key: "verification_code_sent"))
+        Text("\(localize(key: "resend_code_in")) \(resendCountdown)")
+        Button(action: createAccount) {
+          Text(localize(key: "resend_code"))
+        }
+        .disabled(resendCountdown > 0)
 
-			}
-			catch let error {
-				print(error)
-				return
-			}
-		}
-	}
-	
-	func confirmAccount() {
-		Task{
-			do {
-				timerOn = false
-				codeSent = false
-				let langCode: String = lang.identifier.uppercased()
-				let registerRequest = RegisterRequestDto(
-					login: login,
-					displayName: login,
-					email: email,
-					password: password,
-					language: langCode
-				)
-				let confirmationResult = try await RegisterClient().registerConfirm(request: registerRequest, code: code)
-				print(confirmationResult)
-				if confirmationResult == "account_created"{
-					confirmed = true
-				}
-				else {
-					errorText=localize(key: confirmationResult)
-				}
-				
-			}
-			catch let error {
-				print(error)
-				return
-			}
-		}
-	}
+        ViewSwiftUIOTPEntry(
+          model: model,
+          number: $code,
+          isDismissKeyboard: $isDismissKeyboard
+        )
+        Button(action: confirmAccount) {
+          Text(localize(key: "confirm_account"))
+        }
+        .buttonStyle(.glass)
+        .disabled(code.count < 6)
+      }
+      if confirmed {
+        Text(localize(key: "account_created"))
+      }
 
-	//	func handleGoogleSignInButton() {
-	//		guard let rootViewController = UIApplication.shared.rootViewController else {
-	//			// Handle error
-	//			return
-	//		}
-	//
-	//		GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
-	//			guard error == nil else { print(error ?? "none");return}
-	//			guard let signInResult = signInResult else { return }
-	//
-	//			let user = signInResult.user
-	//			//let emailAddress = user.profile?.email
-	//			signInResult.user.refreshTokensIfNeeded { user, error in
-	//				guard error == nil else { return }
-	//				guard let user = user else { return }
-	//
-	//				let idToken = user.idToken
-	//				print(idToken)
-	//				// Send ID token to backend (example below).
-	//			}
-	//		}
-	//	}
+      LabelledDivider(label: localize(key: "or"))
+      //			GoogleSignInButton(
+      //				scheme: .light,
+      //				state: .normal,
+      //				action: {handleGoogleSignInButton()})
+      //			.frame(width: 280, height: 45)
+      //
+      //			SignInWithAppleButton(.continue){
+      //				request in request.requestedScopes=[.email]
+      //			} onCompletion: { result in
+      //				switch result {
+      //					case .success(let authorization):
+      //						print("\(localize(key: "authorization_successful")): \(authorization)")
+      //					case .failure(let error):
+      //						print("\(localize(key: "authorization_failed"))): \(error.localizedDescription)")
+      //				}
+      //			}.frame(width: 280, height: 45)
 
-	func localize(key: String) -> String {
-		return LocalText().getStringDesc(key: key).localized()
-	}
+      //Text(localize(key: "special_sign_in_later"))
+      //Spacer()
+      HStack {
+        Text(localize(key: "already_have_account"))
+        Button(localize(key: "sign_in")) {}
+      }
+
+    }
+    .padding(.top, 15)
+    .onReceive(
+      timer.autoconnect(),
+      perform: { _ in
+        if timerOn {
+          if resendCountdown > 0 {
+            resendCountdown = resendCountdown - 1
+          } else {
+            resendCountdown = 60
+            timerOn = false
+          }
+        }
+      }
+    )
+  }
+
+  func validateFields() {
+    Task {
+      let registerValidator = RegisterValidator()
+      do {
+        let loginValid = try await registerValidator.validateLogin(
+          login: login
+        )
+        if loginValid != nil {
+          errorText = localize(key: loginValid!)
+          return
+        }
+
+        let emailValid = try await registerValidator.validateEmail(
+          email: email
+        )
+        if emailValid != nil {
+          errorText = localize(key: emailValid!)
+          return
+        }
+
+        if password != confirmPassword {
+          errorText = localize(key: "passwords_dont_match")
+          return
+        }
+
+        let passwordValid = registerValidator.isPasswordValid(
+          password: password
+        )
+        if passwordValid != true {
+          errorText = localize(key: "invalid_password")
+        }
+        errorText = ""
+      } catch let error {
+        print(error)
+        return
+      }
+      createAccount()
+    }
+  }
+
+  func createAccount() {
+    Task {
+      do {
+        let langCode: String = lang.identifier.uppercased()
+        let registerRequest = RegisterRequestDto(
+          login: login,
+          displayName: login,
+          email: email,
+          password: password,
+          language: langCode
+        )
+        let result = try await RegisterClient().register(
+          request: registerRequest
+        )
+        if result == "verification_code_sent" {
+          codeSent = true
+          timerOn = true
+        }
+
+      } catch let error {
+        print(error)
+        return
+      }
+    }
+  }
+
+  func confirmAccount() {
+    Task {
+      do {
+        timerOn = false
+        codeSent = false
+        let langCode: String = lang.identifier.uppercased()
+        let registerRequest = RegisterRequestDto(
+          login: login,
+          displayName: login,
+          email: email,
+          password: password,
+          language: langCode
+        )
+        let confirmationResult = try await RegisterClient().registerConfirm(
+          request: registerRequest, code: code)
+        print(confirmationResult)
+        if confirmationResult == "account_created" {
+          confirmed = true
+        } else {
+          errorText = localize(key: confirmationResult)
+        }
+
+      } catch let error {
+        print(error)
+        return
+      }
+    }
+  }
+
+  //	func handleGoogleSignInButton() {
+  //		guard let rootViewController = UIApplication.shared.rootViewController else {
+  //			// Handle error
+  //			return
+  //		}
+  //
+  //		GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
+  //			guard error == nil else { print(error ?? "none");return}
+  //			guard let signInResult = signInResult else { return }
+  //
+  //			let user = signInResult.user
+  //			//let emailAddress = user.profile?.email
+  //			signInResult.user.refreshTokensIfNeeded { user, error in
+  //				guard error == nil else { return }
+  //				guard let user = user else { return }
+  //
+  //				let idToken = user.idToken
+  //				print(idToken)
+  //				// Send ID token to backend (example below).
+  //			}
+  //		}
+  //	}
+
+  func localize(key: String) -> String {
+    return LocalText().getStringDesc(key: key).localized()
+  }
 }
 
 #Preview {
-	RegisterView().preferredColorScheme(.dark)
+  RegisterView().preferredColorScheme(.dark)
 }
--- client/sharedLogic/src/commonMain/kotlin/com/example/client/LocalText.kt
+++ client/sharedLogic/src/commonMain/kotlin/com/example/client/LocalText.kt
@@ -145,6 +145,10 @@ class LocalText {
             "account_created"->MR.strings.account_created
             "invalid_credentials"->MR.strings.invalid_credentials
             "password_changed"->MR.strings.password_changed
+            "resend_code_in"->MR.strings.resend_code_in
+            "resend_code"->MR.strings.resend_code
+            "confirm_account"->MR.strings.confirm_account
+            "done"->MR.strings.done
             else->throw IllegalArgumentException("Unknown string resource: $resourceKey")
         }
     }
