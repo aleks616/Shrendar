29eaf5aaec961e84035c7eba9251e8e3a2a0b958
aleks616 <aleksjankowiak5230@gmail.com>
2026-09-26 18:54:51 +0200

    fix

--- client/sharedUI/src/commonTest/kotlin/com/example/client/account/screens/WelcomeViewTest.kt
+++ client/sharedUI/src/commonTest/kotlin/com/example/client/account/screens/WelcomeViewTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.runComposeUiTest
